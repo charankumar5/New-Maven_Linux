441bed8c9d4572ba6c43d5281140d4efd095cd3a
root <root@localhost.localdomain>
2022-12-08 19:25:17 +0100

    Sending all files to GIT from Maven

---
+++ src/test/java/charan/AppTest.java
@@ -0,0 +1,20 @@
+package charan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
